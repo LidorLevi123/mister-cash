979d94cd98039c7c76a098a17657f41371e195b0
LidorLevi123 <lidorlevi728@gmail.com>
2024-03-30 15:36:52 +0300

    add contact list

---
+++ src/cmps/ContactList.jsx
@@ -0,0 +1,15 @@
+import { ContactPreview } from './ContactPreview'
+
+export function ContactList({ contacts }) {
+    return (
+        <ul className="contact-list clean-list">
+            {
+                contacts.map(contact =>
+                    <li key={contact._id}>
+                        <ContactPreview contact={contact}/>
+                    </li>
+                )
+            }
+        </ul>
+    )
+}
