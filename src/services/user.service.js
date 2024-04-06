b1d45ba93579dcbfad8356d7f57b4b62adfa30cc
LidorLevi123 <lidorlevi728@gmail.com>
2024-04-06 19:24:02 +0300

    add more functionality to user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,41 @@
+import { storageService } from "./async-storage.service"
+
+const STORAGE_KEY_LOGGEDIN_USER = 'loggedInUser'
+
 export const userService = {
-    getUser
+    login,
+    logout,
+    signup,
+    getLoggedInUser,
+    saveLocalUser,
 }
 
-const loggedInUser = {
-    name: 'Lidor Levi',
-    coins: 70233,
-    moves: []
+async function login(userCred) {
+    const users = await storageService.query('user')
+    const user = users.find(user => user.username === userCred.username)
+    if (user) {
+        return saveLocalUser(user)
+    }
 }
 
-function getUser() {
-    return loggedInUser
+async function signup(userCred) {
+    userCred.moves = []
+    userCred.balance = 500
+    if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
+    const user = await storageService.post('user', userCred)
+    return saveLocalUser(user)
+}
+
+function logout() {
+    sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+}
+
+function saveLocalUser(user) {
+    user = {_id: user._id, fullname: user.fullname, email: user.email, imgUrl: user.imgUrl, balance: user.balance, moves: user.moves}
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
+    return user
+}
+
+function getLoggedInUser() {
+    return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
 }
