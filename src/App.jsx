06d967313567aa3819872d32e9df27db5abb6c6d
LidorLevi123 <lidorlevi728@gmail.com>
2024-04-07 19:24:12 +0300

    add route guards

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import './assets/styles/main.scss'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { userService } from './services/user.service'
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
 import { AppHeader } from './cmps/AppHeader'
 import { HomePage } from './pages/HomePage'
 import { StatisticPage } from './pages/StatisticPage'
+import { LoginPage } from './pages/LoginPage'
 import { ContactPage } from './pages/ContactPage'
 import { ContactDetails } from './pages/ContactDetails'
 import { ContactEdit } from './pages/ContactEdit'
 
-function App() {
+function RouteGuard({ children }) {
+    const user = userService.getLoggedInUser() 
+    if (!user) return <Navigate to='/login' />
+    return <>{children}</>
+}
 
+function App() {
     return (
         <>
             <Router>
                 <section className='main-app main-layout'>
                     <AppHeader />
                     <Routes>
-                        <Route path='/' element={<HomePage />} />
+                        <Route path='/' element={<RouteGuard><HomePage /></RouteGuard>} />
                         <Route path='/statistic' element={<StatisticPage />} />
-                        <Route path='/contact' element={<ContactPage />} />
-                        <Route path='/contact/:contactId' element={<ContactDetails />} />
-                        <Route path='/contact/edit/:contactId?' element={<ContactEdit />} />
+                        <Route path='/contact' element={<RouteGuard><ContactPage /></RouteGuard>} />
+                        <Route path='/contact/:contactId' element={<RouteGuard><ContactDetails /></RouteGuard>} />
+                        <Route path='/contact/edit/:contactId?' element={<RouteGuard><ContactEdit /></RouteGuard>} />
+                        <Route path='/login' element={<LoginPage />} />
                     </Routes>
                 </section>
             </Router>
